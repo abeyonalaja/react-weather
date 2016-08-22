45cc0b31a968cba11fa023abe930752f6d069f9b
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-22 01:38:25 +0100

    Add stub component for weather message

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import WeatherForm from './WeatherForm';
+import WeatherMessage from './WeatherMessage';
 
 export default function Weather() {
   return (
     <div className="">
       <h3>Weather Component</h3>
       <WeatherForm />
+      <WeatherMessage />
     </div>
   );
 };
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function WeatherMessage() {
+  return (
+    <h3>Weather Message Component</h3>
+  );
+};
