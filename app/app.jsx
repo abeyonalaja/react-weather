6e02f01bd26490ce364594895a22a5d1b0c83aaa
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-22 01:28:59 +0100

    Add examoples component and indexLink tag with styles

--- app/app.jsx
+++ app/app.jsx
@@ -4,12 +4,14 @@ import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import Main from './components/Main';
 import Weather from './components/Weather';
 import About from './components/About';
+import Examples from './components/Examples';
 
 ReactDom.render(
   <Router history={hashHistory}>
     <Route path="/" component={ Main }>
       <IndexRoute component={ Weather } />
       <Route path="about" component={ About } />
+      <Route path="examples" component={ Examples } />
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,11 +1,13 @@
 import React,{ Component } from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 export default function Nav (){
   return(
     <div className="">
       <h2>Nav Component</h2>
-      <Link>Get Weather</Link>
+      <IndexLink to="/" activeClassName="active" activeStyle={ { fontWeight: 'bold' } } >Get Weather</IndexLink>
+      <Link to="/about" activeClassName="active" activeStyle={ { fontWeight: 'bold' } }>About</Link>
+      <Link to="/examples" activeClassName="active" activeStyle={ { fontWeight: 'bold' } }>Examples</Link>
     </div>
   );
 }
