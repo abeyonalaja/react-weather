e4bf8d19b56a76cf924cc3e7066acca660d4f395
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-22 01:12:27 +0100

    Add about stub component

--- app/app.jsx
+++ app/app.jsx
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import Main from './components/Main';
 import Weather from './components/Weather';
+import About from './components/About';
 
 ReactDom.render(
   <Router history={hashHistory}>
     <Route path="/" component={ Main }>
       <IndexRoute component={ Weather } />
+      <Route path="about" component={ About } />
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,7 +1,11 @@
 import React,{ Component } from 'react';
+import { Link } from 'react-router';
 
 export default function Nav (){
   return(
-    <h2>Nav Component</h2>
+    <div className="">
+      <h2>Nav Component</h2>
+      <Link>Get Weather</Link>
+    </div>
   );
 }
